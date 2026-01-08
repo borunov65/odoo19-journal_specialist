7f2f78dc58223dc6b46effc219a07f19ad0ce642
Ihor_Borunov <ihor_borunov@pop-os.fritz.box>
2026-01-08 16:46:32 +0100

    Fix manifest file

--- __manifest__.py
+++ __manifest__.py
@@ -8,8 +8,9 @@
     "data": [
         "security/ir.model.access.csv",
         "security/journal_rules.xml",
-        "views/journal_entry_views.xml",
         "views/journal_template_views.xml",
+        'views/journal_template_menu.xml',
+        "views/journal_entry_views.xml",
         "views/res_partner_views.xml",
     ],
     "installable": True,
