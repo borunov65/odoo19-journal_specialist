a4b59a87803fcebb909de04d12daa2b0c40fd5ba
Ihor_Borunov <ihor_borunov@pop-os.fritz.box>
2026-01-08 21:00:35 +0100

    Add a method to print to ResPartner model

--- models/res_partner.py
+++ models/res_partner.py
@@ -9,3 +9,8 @@ class ResPartner(models.Model):
         "partner_id",
         string="Specialist Journal"
     )
+
+    def print_journal_report(self):
+        self.ensure_one()
+        journal_entries = self.env['specialist.journal.entry'].search([('partner_id', '=', self.id)])
+        return self.env.ref('journal_specialist.action_report_specialist_journal').report_action(journal_entries)
